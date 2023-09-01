9ff2f0f82ef9e0bb46bf92a04304e4b731faf925
Siddeshwar <siddeshwar.work@gmail.com>
2023-09-01 21:47:14 +0530

    dummy template for home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,57 @@
 @extends('layouts.main')
 
 @section('title') Home @endsection
+@section('head')
+<style>
+    a.movie-link {
+        text-decoration:none;
+        color: black;
+    }
+    .movie-card {
+        border: 1px solid #ddd;
+        margin-bottom: 20px;
+        padding: 10px;
+    }
+    .movie-card img {
+        max-width: 100%;
+        height: auto;
+    }
+    .movie-title {
+        font-weight: bold;
+        font-size: 1.2rem;
+        margin-top: 10px;
+    }
+    .star-rating {
+        color: #FFC107;
+        font-size: 1.2rem;
+        margin-top: 5px;
+    }
+</style>
+@endsection
 @section('content')
-    <h2>Home</h2>
+    <h2 class="py-3">Top movies</h2>
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <a href="movie-details.html" class="movie-link">
+                <div class="movie-card">
+                    <img src="https://placehold.co/300x450" alt="Movie Poster">
+                    <div class="movie-title">Movie Title 1</div>
+                    <div class="star-rating">
+                        ★ 4.2
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <a href="movie-details.html" class="movie-link">
+                <div class="movie-card">
+                    <img src="https://placehold.co/300x450" alt="Movie Poster">
+                    <div class="movie-title">Movie Title 1</div>
+                    <div class="star-rating">
+                        ★ 4.2
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Movie discourse</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    @yield('head') 
 </head>
 <body>
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
