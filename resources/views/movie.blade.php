56402ad4a83ff5284a85e767cd9c14b9e4d747fa
Siddeshwar <siddeshwar.work@gmail.com>
2023-09-02 11:07:18 +0530

    create template for displaying reviews

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -61,6 +61,49 @@
     .description {
         padding-right: 18px;
     }
+    .comment-list {
+        display: block;
+        list-style-type: none;
+        padding-left: 0;
+    }
+
+    .comment {
+        margin-bottom: 20px;
+    }
+
+    .comment .comment-list {
+        margin-top: 15px;
+        margin-left: 50px;
+    }
+
+    .comment-user {
+        display: flex;
+        align-items: center;
+    }
+
+    .comment-avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+        background-color: #ccc;
+    }
+
+    .comment-content {
+        background-color: #f5f5f5;
+        padding: 10px;
+        border-radius: 5px;
+        margin-left: 50px;
+    }
+
+    .comment-actions {
+        margin-top: 10px;
+    }
+
+    .reply-button {
+        color: #007bff;
+        cursor: pointer;
+    }
 </style>
 @endsection
 @section('content')
@@ -111,7 +154,41 @@
     </div>
     <p class="description">When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.</p>
 
-    <!-- Comments -->
-    <h5>Reviews</h5>
 </div>
+
+    <!-- Comments -->
+    <h5 class="my-2">Reviews</h5>
+    <ul class="comment-list">
+            <!-- Root comments -->
+            <li class="comment">
+                <div class="comment-user">
+                    <img src="/assets/profile-picture.png" alt="User Avatar" class="comment-avatar">
+                    <span class="comment-username">User1</span>
+                </div>
+                <div class="comment-content">
+                    <p>This is a top-level comment.</p>
+                    <div class="comment-actions">
+                        <span class="reply-button">Reply</span>
+                    </div>
+                </div>
+
+                <!-- Nested comments -->
+                <ul class="comment-list">
+                    <li class="comment">
+                        <div class="comment-user">
+                            <img src="/assets/profile-picture.png" alt="User Avatar" class="comment-avatar">
+                            <span class="comment-username">User2</span>
+                        </div>
+                        <div class="comment-content">
+                            <p>This is a nested comment.</p>
+                            <div class="comment-actions">
+                                <span class="reply-button">Reply</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Add more comments here as needed -->
+        </ul>
 @endsection
